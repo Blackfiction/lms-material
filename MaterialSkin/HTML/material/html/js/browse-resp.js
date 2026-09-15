079a7e448b052de35dea8d60dcb91574ad512913
CDrummond <craig.p.drummond@gmail.com>
2026-09-15 16:52:24 +0100

    Don't fire 'Rescan album' twice if it needed to be scolled into view. Issue #1266

--- MaterialSkin/HTML/material/html/js/browse-resp.js
+++ MaterialSkin/HTML/material/html/js/browse-resp.js
@@ -883,6 +883,7 @@ function parseBrowseResp(data, parent, options, cacheKey) {
             }
             if (1==resp.items.length && 'text'==resp.items[0].type && 'itemNoAction'==resp.items[0].style && msgIsEmpty(resp.items[0].title)) {
                 resp.items=[];
+                resp.listSize=0;
             }
 
             if (isAppsTop) {
